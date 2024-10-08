475800d34eac398405d2be6da6e9bc3f564ec217
rajaonahsoa <r.lalainafr@gmail.com>
2024-10-08 15:56:50 +0200

    User registration with email confirmation to activate user

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register', views.register_user, name='register-user'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
 ]
--- users/views.py
+++ users/views.py
@@ -5,28 +5,66 @@ from users.models import User
 from .form import ResigterUserForm
 from django.contrib.auth.forms import *
 from django.core.mail import send_mail
+from django.views.generic import View
 
+# token - mail confirmation import
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from .token import TokenGenerator, generate_token
+from django.utils.encoding import force_bytes, force_str
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        # on decode le pk et on le compare si ca correspond à l'id du user
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except Exception as identifier:
+        user = None
+        
+    # on decode le token et le compare par rapport au token generé   
+    if user is not None and generate_token.check_token(user, token):
+        user.is_active = True
+        user.save()     
+        messages.success(request, f'Votre compte a été activé, pour pouvez vous authentifier actuelement..')
+        return redirect('login') 
+    return render (request, 'users/activate_fail.html')  
+    
 
 # registration
 def register_user(request):
     if request.method == 'POST':
         form = ResigterUserForm(request.POST)
         if form.is_valid():
-            var = form.save(commit=False)
-            if var.is_engineer:
-                var.is_engineer =  True
+            user = form.save(commit=False)
+            user.is_active = False
+            if user.is_engineer:
+                user.is_engineer =  True
             else:
-                var.is_customer =  True
+                user.is_customer =  True
     
-            var.save()
-            messages.success(request, 'Votre compte a été créé avec succès. Merci de vous authentifier...')
+            user.save()
+            
+            # email content
+            email_subject="Activer votre compte"
+            message = render_to_string('users/activate.html', {
+                'user ': user,
+                'domain':'127.0.0.1:8000',
+                # encoder le user.pk
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                # generer un token pour le user.pk
+                'token': generate_token.make_token(user)
+            })
+            
+            messages.success(request, f'Votre compte a été créé avec succès. Merci de confirmer votre compte dans le mail qui vous a été envoyé..')
             
              # send mail
-            subject = f'Activation du compte de {var.username} #{var.id}'
-            message =  "Merci d'avoir créé votre compte ...  Afind de l'activer merci de cliquer sur le lien suivant"
             email_from = 'lalaina@myself.com'
-            recipient_list = [var.email]
-            send_mail(subject, message, email_from, recipient_list)
+            recipient_list = [user.email]
+            send_mail(email_subject, message, email_from, recipient_list)
             
             return redirect('login')
         else:
